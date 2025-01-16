856e6f1879d54b6cb6e6637a073621eb7540c7fb
Altan <albert@altan.ai>
2025-01-16 13:17:57 +0100

    Update header title to 'Altan Travels'

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ import NotFound from "./pages/NotFound";
 import DashboardPage from "./pages/dashboard/DashboardPage";
 import { useTheme } from "./theme/use-theme";
 
-const appName = "My app";
+const appName = "Altan Travels";
 
 // Define sidebar configuration
 const dashboardSidebar = {
